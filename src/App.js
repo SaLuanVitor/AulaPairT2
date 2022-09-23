6a1c5b4e133bb59f9a87ef914139564689b9449b
Franciane Lark <franciane.lark019@gmail.com>
2022-09-23 14:15:56 -0400

    update paragrafo

--- src/App.js
+++ src/App.js
@@ -14,12 +14,16 @@ border:dashed 3px red;
 h1{
   color:white;
 }
+p{
+  color: pink;
+}
 `
 
 export default function App() {
   return (
     <Header>
-      <h1>Oi pessoal!!!!!</h1>  
+      <h1>Oi pessoal!!!!!</h1>
+      <p>Saudações turma 02</p>  
     </Header>
   )
 }
